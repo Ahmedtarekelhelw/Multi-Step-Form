638186866f0ab4493db9e74a9b0d4b94454f50a9
Ahmedtarekelhelw <elhelwahmed4@gmail.com>
2022-03-12 10:13:15 +0200

    make some changes

--- src/components/Form/style.js
+++ src/components/Form/style.js
@@ -1,17 +1,19 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
-
-export const Form =styled.form`
-    display: flex;
-    flex-direction: column;
-    width: 50%;
-    margin: 30px auto;
-`
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  margin: 30px auto;
+  @media (max-width: 768px) {
+    width: 90%;
+  }
+`;
 
 export const Input = styled.input`
-    outline: none;
-    margin-bottom: 35px;
-    border: none;
-    padding: 10px;
-    border-radius: 3px;
-`
+  outline: none;
+  margin-bottom: 35px;
+  border: none;
+  padding: 10px;
+  border-radius: 3px;
+`;
